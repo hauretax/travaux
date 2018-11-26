df2ce7ede5082001c47d21216b219b0417bf0ea1
Huggo TRICOTTET <hutricot@e3r11p13.42.fr>
2018-11-26 13:46:26 +0100

    d

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,14 +6,31 @@
 /*   By: hutricot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 08:57:26 by hutricot          #+#    #+#             */
-/*   Updated: 2018/11/22 17:08:30 by hutricot         ###   ########.fr       */
+/*   Updated: 2018/11/26 13:43:54 by hutricot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int			g_n_l(const int fd, char **line, char **sav,
-				char buf[BUFF_SIZE + 1])
+static int		bonus(char **line, char **sav)
+{
+	char	*str;
+
+	str = ft_strdup(*line);
+	ft_free(*line, NULL);
+	*line = ft_strsub(str, 0, ft_f_c(*line, '\n'));
+	if (str[0] == '\n')
+	{
+		*line = (char *)malloc(1);
+		*sav = ft_strdup(str);
+	}
+	ft_free(str, NULL);
+	(*sav)++;
+	return (1);
+}
+
+static int		g_n_l(const int fd, char **line, char **sav,
+					char buf[BUFF_SIZE + 1])
 {
 	int		ret;
 
@@ -22,30 +39,29 @@ int			g_n_l(const int fd, char **line, char **sav,
 		ft_splonchr(line, sav, *sav);
 	if (ft_f_c(*sav, '\n'))
 		return (1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) && ft_strlen(*sav) == 0)
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
 		*line = ft_strjoin_f(*line, buf);
-		ft_free(*sav, NULL);
 		if ((*sav = ft_strchr(*line, '\n')))
-		{
-			*line = ft_strsub(*line, 0, ft_f_c(*line, '\n'));
-			(*sav)++;
-			return (1);
-		}
+			return (bonus(line, sav));
 	}
 	return (0);
 }
 
-int			get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
 	static char	*sav;
 	char		buf[BUFF_SIZE + 1];
 	int			i;
 
+	i = 0;
 	if (read(fd, buf, 0))
 		return (-1);
 	i = g_n_l(fd, line, &sav, buf);
+	if (*line != NULL)
+		if (*line[0] != '\0')
+			i = 1;
 	if (ft_strequ(*line, "\n"))
 		*line = (char *)malloc(1);
 	if (i == 0)
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: hutricot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 13:41:41 by hutricot          #+#    #+#             */
-/*   Updated: 2018/11/22 14:50:28 by hutricot         ###   ########.fr       */
+/*   Updated: 2018/11/23 11:23:39 by hutricot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 4
 # include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
